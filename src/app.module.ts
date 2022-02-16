26e2871ae8c5c35d4aea8329b9f769c26da81417
Leona Okafor <60227761+Leona-cloud@users.noreply.github.com>
2022-02-16 16:54:51 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { TodoController } from './todo/todo.controller';
 import { TodoModule } from './todo/todo.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TodoService } from './todo/todo.service';
+import { Todo, TodoSchema } from './todo/schemas/todo.schema';
+
+
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'),TodoModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'),TodoModule, MongooseModule.forFeature([{name: Todo.name, schema: TodoSchema}]) ],
   controllers: [AppController, TodoController],
-  providers: [AppService],
+  providers: [AppService, TodoService],
 })
 export class AppModule {}
